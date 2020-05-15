122c07402707e14be68e6de93632a19c7eede82f
Danh Nhan Phung <danhnhan.phung@gmail.com>
2020-05-15 15:30:31 +0200

    add profile example

---
+++ src/main/java/com/example/springdi/springdi/controllers/I18nController.java
@@ -0,0 +1,19 @@
+package com.example.springdi.springdi.controllers;
+
+import com.example.springdi.springdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class I18nController {
+
+    public final GreetingService greetingService;
+
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayGreeting() {
+        return greetingService.getGreeting();
+    }
+}
